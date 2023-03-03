299a3022298aa6b7822cbc6350ad3d161e95e8d6
tanju18ctg <tanju18ctg@gmail.com>
2023-03-03 21:40:53 +0600

    first uploaded

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+import { createApp } from 'vue'
+// import the root component App from a single-file component.
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
